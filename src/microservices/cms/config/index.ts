67ed32fb1894f6b38a794e208949876b05c54eff
tam11a <ibrahimsadiktamim@gmail.com>
2023-07-25 21:50:04 +0600

    updated: config for db

--- src/microservices/cms/config/index.ts
+++ src/microservices/cms/config/index.ts
@@ -10,7 +10,7 @@ module.exports = {
 	database: {
 		dialect: "mysql",
 		host: process.env.DB_HOST,
-		user: process.env.DB_USER,
+		username: process.env.DB_USER,
 		password: process.env.DB_PASSWORD,
 		database: process.env.DB_NAME,
 	},
