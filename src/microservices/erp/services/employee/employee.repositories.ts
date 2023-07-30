57b5fa8b520330070becdd85f2a47bec0c943105
sanchitadevi <thesanchitadevi@gmail.com>
2023-07-31 00:53:44 +0600

    employee list

--- src/microservices/erp/services/employee/employee.repositories.ts
+++ src/microservices/erp/services/employee/employee.repositories.ts
@@ -1,12 +1,11 @@
 import { Request, Response, NextFunction } from "express";
 import Employee from "./employee.model";
+import { Op } from "sequelize";
+import Pagination from "@/utils/Pagination";
 
 class EmployeeRepository {
   constructor() {}
 
-  //create
-  //findandcountall
-  //update
   public async create(req: Request, res: Response, next: NextFunction) {
     try {
       await Employee.create(
@@ -43,13 +42,116 @@ class EmployeeRepository {
     }
   }
 
-  public async find(_req: Request, res: Response, _next: NextFunction) {
-    console.log(await Employee.findOne());
-    res.send("Employee Find API");
+  public async find(req: Request, res: Response, next: NextFunction) {
+    const pagination = new Pagination(req, res, next);
+    const { offset, limit } = pagination.get_attributes();
+
+    pagination.arrange_and_send(
+      await Employee.findAndCountAll({
+        where: {
+          [Op.or]: [
+            {
+              first_name: {
+                [Op.like]: `%${pagination.search_string}%`,
+              },
+            },
+            {
+              last_name: {
+                [Op.like]: `%${pagination.search_string}%`,
+              },
+            },
+            {
+              username: {
+                [Op.like]: `%${pagination.search_string}%`,
+              },
+            },
+            {
+              phone: {
+                [Op.like]: `%${pagination.search_string}%`,
+              },
+            },
+          ],
+        },
+        attributes: {
+          exclude: ["password"],
+        },
+        offset,
+        limit,
+      })
+    );
   }
 
-  public delete(req: Request, res: Response, _next: NextFunction) {
-    res.send(`Employee ${req.params.id} Delete API`);
+  public async findById(req: Request, res: Response, next: NextFunction) {
+    try {
+      if (!req.params.id)
+        return next(new ErrorResponse("No employee found!", 404));
+
+      var employee = await Employee.findByPk(req.params.id, {
+
+        attributes: {
+          exclude: ["password"],
+        },
+      });
+
+      res.status(201).json({
+        success: true,
+        message: "Information fetched successfully",
+        data: employee,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  public async update(req: Request, res: Response, next: NextFunction) {
+    try {
+      if (!req.params.id)
+      return next(new ErrorResponse("Invalid Request!", 400));
+      const {
+            first_name,
+            last_name,
+            username,
+            gender,
+            display_picture,
+            email,
+            phone,
+            dob,
+            hired_date,
+            role,
+            work_hour,
+            salary,
+            bank,
+            default_address
+      } = req.body;
+     
+      var employee = await Employee.findByPk(req.params.id, {});
+      
+      if (!employee) return next(new ErrorResponse("No employee found!", 404));
+      
+      await employee.update({
+        first_name,
+        last_name,
+        username,
+        gender,
+        display_picture,
+        email,
+        phone,
+        dob,
+        hired_date,
+        role,
+        work_hour,
+        salary,
+        bank,
+        default_address,
+      });
+
+      res.status(200).json({
+        success: true,
+        message: "Information updated successfully",
+      });
+    } catch (error) {
+      next(error);
+    }
   }
 }
 
--- src/microservices/erp/services/employee/index.ts
+++ src/microservices/erp/services/employee/index.ts
@@ -4,11 +4,9 @@ import EmployeeRepository from "./employee.repositories";
 const repository = new EmployeeRepository();
 
 employee_router.post("/", [repository.create]);
-employee_router.get("/");
-employee_router.patch("/:id");
-employee_router.get("/:id");
-employee_router.delete("/:id");
-
+employee_router.get("/", [repository.find]);
+employee_router.get("/:id", [repository.findById]);
+employee_router.patch("/:id", [repository.update]);
 
 module.exports = employee_router;
 
