a164c9493f670e7b276da09a8f17bd0123e6d9d5
sanchitadevi <thesanchitadevi@gmail.com>
2023-08-03 02:31:17 +0600

    active status base search

--- src/microservices/cms/services/users/users.repositories.ts
+++ src/microservices/cms/services/users/users.repositories.ts
@@ -7,33 +7,47 @@ import Database from "@/database";
 const User = Database.get_model("User");
 
 class UserRepository {
-	constructor() {}
+  constructor() {}
 
-	public async find(req: Request, res: Response, next: NextFunction) {
-		const { get_attributes, get_search_ops, arrange_and_send } = new Pagination(
-			req,
-			res,
-			next
-		);
+  public async find(req: Request, res: Response, next: NextFunction) {
+    // Query Props
+    const { gender, is_active } = req.query;
 
-		const { offset, limit } = get_attributes();
-		const search_ops = get_search_ops([
-			"first_name",
-			"last_name",
-			"username",
-			"phone",
-		]);
+    // Pagination, Filter, Search
+    const {
+      get_attributes,
+      get_search_ops,
+      format_filters,
+      arrange_and_send,
+      toBoolean,
+    } = new Pagination(req, res, next);
 
-		arrange_and_send(
-			await User.findAndCountAll({
-				where: {
-					[Op.or]: search_ops,
-				},
-				attributes: {
-					exclude: ["password", "default_address"],
-					include: [
-						[
-							Database.sequelize.literal(`(
+    // Get Props for Query
+    const { offset, limit, paranoid } = get_attributes();
+    // Get Search Object
+    const search_ops = get_search_ops([
+      "first_name",
+      "last_name",
+      "username",
+      "phone",
+    ]);
+    // Get Filter Props
+    const filters = format_filters({
+      gender,
+      is_active: toBoolean(is_active),
+    });
+
+    arrange_and_send(
+      await User.findAndCountAll({
+        where: {
+          [Op.or]: search_ops,
+          ...filters,
+        },
+        attributes: {
+          exclude: ["password", "default_address"],
+          include: [
+            [
+              Database.sequelize.literal(`(
 								SELECT COUNT(*)
 								FROM user_session AS session
 								WHERE
@@ -41,95 +55,96 @@ class UserRepository {
 									AND
 									session.logged_out_at IS NOT NULL
 							)`),
-							"active_sessions",
-						],
-					],
-				},
-				offset,
-				limit,
-			})
-		);
-	}
+              "active_sessions",
+            ],
+          ],
+        },
+        offset,
+        limit,
+        paranoid, // if False, shows soft-deleted data too
+      })
+    );
+  }
 
-	public async findById(req: Request, res: Response, next: NextFunction) {
-		try {
-			var user = await User.findByPk(req.params.id, {
-				attributes: {
-					exclude: ["password"],
-				},
-			});
-			if (!user) return next(new ErrorResponse("No user found!", 404));
+  public async findById(req: Request, res: Response, next: NextFunction) {
+    try {
+      var user = await User.findByPk(req.params.id, {
+        attributes: {
+          exclude: ["password"],
+        },
+      });
+      if (!user) return next(new ErrorResponse("No user found!", 404));
 
-			res.status(200).json({
-				success: true,
-				message: "Information fetched successfully",
-				data: user,
-			});
-		} catch (error) {
-			next(error);
-		}
-	}
+      res.status(200).json({
+        success: true,
+        message: "Information fetched successfully",
+        data: user,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
 
-	public async update(req: Request, res: Response, next: NextFunction) {
-		try {
-			if (!req.params.id)
-				return next(new ErrorResponse("Invalid Request!", 400));
-			const {
-				first_name,
-				last_name,
-				username,
-				gender,
-				display_picture,
-				email,
-				default_address,
-				max_session,
-			} = req.body;
+  public async update(req: Request, res: Response, next: NextFunction) {
+    try {
+      if (!req.params.id)
+        return next(new ErrorResponse("Invalid Request!", 400));
+      const {
+        first_name,
+        last_name,
+        username,
+        gender,
+        display_picture,
+        email,
+        default_address,
+        max_session,
+      } = req.body;
 
-			var user = await User.findByPk(req.params.id, {});
+      var user = await User.findByPk(req.params.id, {});
 
-			if (!user) return next(new ErrorResponse("No user found!", 404));
+      if (!user) return next(new ErrorResponse("No user found!", 404));
 
-			await user.update({
-				first_name,
-				last_name,
-				username,
-				gender,
-				display_picture,
-				email,
-				default_address,
-				max_session,
-			});
+      await user.update({
+        first_name,
+        last_name,
+        username,
+        gender,
+        display_picture,
+        email,
+        default_address,
+        max_session,
+      });
 
-			res.status(204).json({
-				success: true,
-				message: "Information updated successfully",
-			});
-		} catch (error) {
-			next(error);
-		}
-	}
+      res.status(204).json({
+        success: true,
+        message: "Information updated successfully",
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
 
-	public async activeInactive(req: Request, res: Response, next: NextFunction) {
-		try {
-			var user = await User.findByPk(req.params.id, {});
+  public async activeInactive(req: Request, res: Response, next: NextFunction) {
+    try {
+      var user = await User.findByPk(req.params.id, {});
 
-			if (!user) return next(new ErrorResponse("No user found!", 404));
+      if (!user) return next(new ErrorResponse("No user found!", 404));
 
-			await user.update({
-				is_active: !user.getDataValue("is_active"),
-			});
-			await user.save();
+      await user.update({
+        is_active: !user.getDataValue("is_active"),
+      });
+      await user.save();
 
-			res.status(204).json({
-				success: true,
-				message: `Employee ${
-					user.getDataValue("is_active") ? "suspended" : "activated"
-				} successfully`,
-			});
-		} catch (error) {
-			next(error);
-		}
-	}
+      res.status(204).json({
+        success: true,
+        message: `Employee ${
+          user.getDataValue("is_active") ? "suspended" : "activated"
+        } successfully`,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 export default UserRepository;
--- src/microservices/cms/utils/Pagination/index.ts
+++ src/microservices/cms/utils/Pagination/index.ts
@@ -2,123 +2,133 @@ import { NextFunction, Request, Response } from "express";
 import { Op, literal } from "sequelize";
 
 class Pagination {
-	private _req: Request;
-	private _res: Response;
-	private _next: NextFunction;
-	public search_string?: string;
-	public page: number = 0;
-	public limit: number = 10;
-	public skip: number = 0;
-	public show_paranoid: boolean = true;
-	public sort?: string;
-	public literal_fields: string[] = [];
+  private _req: Request;
+  private _res: Response;
+  private _next: NextFunction;
+  public search_string?: string;
+  public page: number = 0;
+  public limit: number = 10;
+  public skip: number = 0;
+  public show_paranoid: boolean = true;
+  public sort?: string;
+  public literal_fields: string[] = [];
 
-	constructor(
-		req: Request,
-		res: Response,
-		next: NextFunction,
-		literal_fields: string[] = []
-	) {
-		this._req = req;
-		this._res = res;
-		this._next = next;
-		try {
-			const { search, limit, page, sort, show_paranoid } = this._req.query;
-			this.sort = sort ? this.replaceAll(sort?.toString(), " ", "") : undefined;
-			this.limit =
-				limit && parseInt(limit?.toString()) ? parseInt(limit?.toString()) : 10;
-			this.search_string = search?.toString() || "";
-			this.page =
-				page && parseInt(page?.toString() || "0") > 1
-					? parseInt(page?.toString() || "0")
-					: 1;
-			this.skip = (this.page - 1) * this.limit;
-			this.literal_fields = literal_fields;
+  constructor(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+    literal_fields: string[] = []
+  ) {
+    this._req = req;
+    this._res = res;
+    this._next = next;
+    try {
+      const { search, limit, page, sort, show_paranoid } = this._req.query;
+      this.sort = sort ? this.replaceAll(sort?.toString(), " ", "") : undefined;
+      this.limit =
+        limit && parseInt(limit?.toString()) ? parseInt(limit?.toString()) : 10;
+      this.search_string = search?.toString() || "";
+      this.page =
+        page && parseInt(page?.toString() || "0") > 1
+          ? parseInt(page?.toString() || "0")
+          : 1;
+      this.skip = (this.page - 1) * this.limit;
+      this.literal_fields = literal_fields;
 
-			this.show_paranoid =
-				show_paranoid !== null && show_paranoid !== undefined
-					? show_paranoid === "true"
-						? true
-						: false
-					: true;
-		} catch (err) {
-			this._next(err);
-		}
+      this.show_paranoid =
+        show_paranoid !== null && show_paranoid !== undefined
+          ? show_paranoid === "true"
+            ? true
+            : false
+          : true;
+    } catch (err) {
+      this._next(err);
+    }
 
-		this.replaceAll = this.replaceAll.bind(this);
-		this.order = this.order.bind(this);
-		this.get_attributes = this.get_attributes.bind(this);
-		this.get_search_ops = this.get_search_ops.bind(this);
-		this.format_filters = this.format_filters.bind(this);
-		this.arrange = this.arrange.bind(this);
-		this.arrange_and_send = this.arrange_and_send.bind(this);
-	}
+    this.replaceAll = this.replaceAll.bind(this);
+    this.order = this.order.bind(this);
+    this.get_attributes = this.get_attributes.bind(this);
+    this.get_search_ops = this.get_search_ops.bind(this);
+    this.format_filters = this.format_filters.bind(this);
+    this.arrange = this.arrange.bind(this);
+    this.arrange_and_send = this.arrange_and_send.bind(this);
+  }
 
-	private replaceAll(str: string, find: string, replace: string) {
-		return str.replace(new RegExp(find, "g"), replace);
-	}
+  private replaceAll(str: string, find: string, replace: string) {
+    return str.replace(new RegExp(find, "g"), replace);
+  }
 
-	public order(literal_fields: string[] | undefined = []) {
-		if (!this.sort) return [];
-		literal_fields = literal_fields.concat(this.literal_fields);
+  public order(literal_fields: string[] | undefined = []) {
+    if (!this.sort) return [];
+    literal_fields = literal_fields.concat(this.literal_fields);
 
-		return Array.from(this.sort, (s: string) =>
-			s[0] === "-"
-				? [
-						literal_fields.includes(s.replace("-", ""))
-							? literal(s.replace("-", ""))
-							: s.replace("-", ""),
-						"ASC",
-				  ]
-				: [literal_fields.includes(s) ? literal(s) : s, "DESC"]
-		);
-	}
+    return Array.from(this.sort, (s: string) =>
+      s[0] === "-"
+        ? [
+            literal_fields.includes(s.replace("-", ""))
+              ? literal(s.replace("-", ""))
+              : s.replace("-", ""),
+            "ASC",
+          ]
+        : [literal_fields.includes(s) ? literal(s) : s, "DESC"]
+    );
+  }
 
-	public get_attributes(literal_fields: string[] | undefined = []) {
-		return {
-			offset: this.skip,
-			limit: this.limit,
-			order: this.order(literal_fields),
-			paranoid: this.show_paranoid,
-		};
-	}
+  public get_attributes(literal_fields: string[] | undefined = []) {
+    return {
+      offset: this.skip,
+      limit: this.limit,
+      order: this.order(literal_fields),
+      paranoid: this.show_paranoid,
+    };
+  }
 
-	public get_search_ops(search_fields: string[] = []) {
-		return Array.from(search_fields, (f: string) => ({
-			[f]: {
-				[Op.like]: `%${this.search_string}%`,
-			},
-		}));
-	}
+  public get_search_ops(search_fields: string[] = []) {
+    return Array.from(search_fields, (f: string) => ({
+      [f]: {
+        [Op.like]: `%${this.search_string}%`,
+      },
+    }));
+  }
 
-	public format_filters(filter_object: { [key: string]: any }) {
-		const output_object: { [key: string]: any } = {};
-		for (const key in filter_object) {
-			if (filter_object[key] !== null && filter_object[key] !== undefined) {
-				output_object[key] = filter_object[key];
-			}
-		}
-		return output_object;
-	}
+  public format_filters(filter_object: { [key: string]: any }) {
+    const output_object: { [key: string]: any } = {};
+    for (const key in filter_object) {
+      if (filter_object[key] !== null && filter_object[key] !== undefined) {
+        output_object[key] = filter_object[key];
+      }
+    }
+    return output_object;
+  }
 
-	public arrange(data: any) {
-		return {
-			success: true,
-			message: "Data fetched sucessfully",
-			data: data.rows,
-			total: data.count,
-			limit: this.limit,
-			page: this.page,
-			has_next_page: Math.ceil(data.count / this.limit) - this.page > 0,
-			has_previous_page: this.page > 1,
-			total_pages: Math.ceil(data.count / this.limit),
-			showing_soft_deleted: !this.show_paranoid,
-		};
-	}
+  public arrange(data: any) {
+    return {
+      success: true,
+      message: "Data fetched sucessfully",
+      data: data.rows,
+      total: data.count,
+      limit: this.limit,
+      page: this.page,
+      has_next_page: Math.ceil(data.count / this.limit) - this.page > 0,
+      has_previous_page: this.page > 1,
+      total_pages: Math.ceil(data.count / this.limit),
+      showing_soft_deleted: !this.show_paranoid,
+    };
+  }
 
-	public arrange_and_send(data: any, status: number = 200) {
-		this._res.status(status).json(this.arrange(data));
-	}
+  public arrange_and_send(data: any, status: number = 200) {
+    this._res.status(status).json(this.arrange(data));
+  }
+
+  public toBoolean(value: any) {
+    return value !== null && value !== undefined
+      ? ["true", "True", true, 1].includes(value)
+        ? true
+        : ["false", "False", false, 0].includes(value)
+        ? false
+        : undefined
+      : undefined;
+  }
 }
 
 export default Pagination;
